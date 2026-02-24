6c154e705f9b399eeebd4391fafd3f5d33303d11
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2026-02-24 21:04:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        //  code here
+        vector<int> freq(26, 0);
+        for(char ch: s){
+            freq[ch - 'a']++;
+        }
+        for(char ch: s){
+            if(freq[ch - 'a'] == 1){
+                return ch;
+            }
+        }
+        return '$';
+    }
+};
