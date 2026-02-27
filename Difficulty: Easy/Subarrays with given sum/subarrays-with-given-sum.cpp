0ce4c631723c56bef413cdf7b6a17c36f0fdb9db
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2026-02-27 21:36:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Subarrays with given sum/subarrays-with-given-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int subArraySum(vector<int>& arr, int k) {
+        // code here.
+        unordered_map<int,int> mp;
+        int count = 0;
+        int sum = 0;
+        mp[0] = 1;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr[i];
+            if(mp.find(sum - k) != mp.end()){
+                count += mp[sum - k];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
