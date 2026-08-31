7c862d608f0f3e6bc8bc471e3832fc22a7ebcd62
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2026-08-31 09:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.53%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        char_set = set()
+        for char in sentence:
+            char_set.add(char)
+        if len(char_set) == 26:
+            return True
+        return False
