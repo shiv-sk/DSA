b9dec3b310b58c530cda1035f005646e447d22dc
shivanand_khudanapur <155430597+shiv-sk@users.noreply.github.com>
2026-09-01 09:10:07 +0530

    Time: 11 ms (77.37%) | Memory: 19.3 MB (76.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            s_map[char] = s_map.get(char, 0) + 1
+        for char in t:
+            if char in s_map and s_map[char] > 0:
+                s_map[char] -= 1
+            else:
+                return False
+        return True
